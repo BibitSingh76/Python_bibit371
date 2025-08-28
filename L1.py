4659af6217da18c24b29569a82d10d24605bc07f
BibitSingh76 <bibitsingh76@gmail.com>
2025-08-28 14:49:44 +0530

    new

---
+++ L1.py
@@ -0,0 +1,30 @@
+# integer data type
+# var1=1
+# print(var1)
+# var2 = True
+# print(var2)
+# var3 =10.023
+# print(var3)
+# var4 =10+3j
+# print(var4)
+# var5 =5
+# print(var5)
+
+# # String data type
+# # multiDataType string
+# a = """ Lorem hello,
+# how are you """
+# print(a)
+# # for finding substring 
+# b="bibit"
+# print("bi" in b)
+
+# slicing string
+# b="bibit"
+# print(b[2:4])
+# print(b[:4])
+# print(b[2:])
+
+c="hello world!"
+print(c.upper())
+print("hello")
