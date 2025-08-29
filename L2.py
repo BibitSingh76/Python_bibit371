773b1f85f1ea9abbe9f40b8bc6d9f88fa84bce21
BibitSingh76 <bibitsingh76@gmail.com>
2025-08-29 15:49:45 +0530

    new comnit

---
+++ L2.py
@@ -0,0 +1,22 @@
+#replacing string 
+a= "hello, world"
+print(a.replace("l","#"))
+
+# for spliting
+
+a="hello world"
+print(a.split())
+
+text = "Hello world Python"
+parts = text.split()
+print(parts)
+
+# formating changing
+age =20
+# formating change karne ke liye f ka use kiya hai
+text =f"my age is + {age}" 
+print(text)
+
+# boolean
+print(bool(25))
+print(bool(5>8))
