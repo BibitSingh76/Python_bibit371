ce64adea0cef49a1c708ca04962cb40e27abcf65
BibitSingh76 <bibitsingh76@gmail.com>
2025-09-04 00:18:11 +0530

    new change

--- L2.py
+++ L2.py
@@ -1,26 +1,29 @@
-#replacing string 
-a= "hello, world"
-print(a.replace("l","#"))
+# check a sub string of a another stirng 
+txt = "The best thinks in life are free!"
+print("free" in txt) #method 1
+if "free" in txt: # method 2 if statement se
+    print("Yes, 'free' is present.")
 
-# for spliting
+print("hello" not in txt) #hello is not present in txt so coming out true
 
-a="hello world"
-print(a.split())
+#Slicing string
+b= "Hello, world!"
+print(b[2:5]) #end index excluding hai and start wala including hai
+print(b[:5]) #by default 0->5 where index 5 is excluding
+print(b[2:]) #by default 2->end
+print(b[-5:-2]) # in -ve index oprate from end 2->5 (end se  OR reverse me)
 
-text = "Hello world Python"
-parts = text.split()
-print(parts)
 
-# formating changing
-age =20
-# formating change karne ke liye f ka use kiya hai
-text =f"my age is + {age}" 
-print(text)
+#Modify Strings
+a,b="bibit","SINGH"
+print(a.upper())
+print(b.lower())
+print(a.strip())
+print(a.replace('b','B')) #replacing
+print(a.split(","))# spliting in word 
 
-# boolean
-print(bool(25))
-print(bool(5>8))
-
-# length of string
-a= "hello, world!"
-print(a[2])
+# Formate String
+age=20
+# txt = "my name is bibit , my age is" + age wrong method
+txt = f"my name is bibit , my age is {age}" #use f syntex in begin
+print(txt)
